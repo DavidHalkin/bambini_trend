b37a6ce4928528fc181c53dd3f50fe261e2d953d
David <northfire@mail.ru>
2017-07-12 20:10:08 +0300

    bower add

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync');
 
 gulp.task('sass',function(){
-	return gulp.src('app/scss/*.scss')
+	return gulp.src('app/sass/**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server:{
+			baseDir:'app'
+		},
+		notify:false
+	});
+});
+
+gulp.task('watch',['browser-sync', 'sass'], function(){
+	gulp.watch('app/sass/**/*.scss',['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.html', browserSync.reload);
 });
