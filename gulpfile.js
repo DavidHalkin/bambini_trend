5c66c79f5b992ee7207a9cd15d0a072a114c365b
David <northfire@mail.ru>
2017-07-12 18:40:06 +0300

    add sass+

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
 
+gulp.task('sass',function(){
+	return gulp.src('app/scss/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+});
