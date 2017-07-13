c10bf5dc0e02ea673b54d54849366e7387afaa0b
David <northfire@mail.ru>
2017-07-13 16:15:53 +0300

    the best of th best

--- gulpfile.js
+++ gulpfile.js
@@ -28,15 +28,15 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 	});
 });
 
-gulp.task('scripts', function() {
-	return gulp.src([ // Берем все необходимые библиотеки
-		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
-		])
-		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
-		.pipe(uglify()) // Сжимаем JS файл
-		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
-});
+// gulp.task('scripts', function() {
+// 	return gulp.src([ // Берем все необходимые библиотеки
+// 		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+// 		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+// 		])
+// 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+// 		.pipe(uglify()) // Сжимаем JS файл
+// 		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+// });
 
 gulp.task('css-libs', ['sass'], function() {
 	return gulp.src('app/css/libs.css') // Выбираем файл для минификации
@@ -45,10 +45,10 @@ gulp.task('css-libs', ['sass'], function() {
 		.pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
 });
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+gulp.task('watch', ['browser-sync', 'css-libs'], function() { //удалил , 'scripts' после цсс-либс
+	gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
 	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
+	// gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
 gulp.task('clean', function() {
@@ -66,7 +66,7 @@ gulp.task('img', function() {
 		.pipe(gulp.dest('dist/images')); // Выгружаем на продакшен
 });
 
-gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+gulp.task('build', ['clean', 'img', 'sass'], function() { //удалил , 'scripts' после сасс
 
 	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
 		'app/css/all.css',
@@ -77,8 +77,8 @@ gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
 	var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
 	.pipe(gulp.dest('dist/fonts'))
 
-	var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
-	.pipe(gulp.dest('dist/js'))
+	// var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+	// .pipe(gulp.dest('dist/js'))
 
 	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
 	.pipe(gulp.dest('dist'));
