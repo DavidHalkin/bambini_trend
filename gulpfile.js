ff23d161030fe39ceb69ba245a67a5c943bab6d1
David <northfire@mail.ru>
2017-07-14 14:16:54 +0300

    add fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('css-libs', ['sass'], function() {
 
 gulp.task('sass', function(){ // Создаем таск Sass
 	return gulp.src('app/sass/**/*.scss') // Берем источник
-		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
